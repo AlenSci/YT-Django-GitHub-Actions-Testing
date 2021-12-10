ed153d1237bf13f45e36d8720f61739c98d537c0
AlenSci <ali.alkaraawi@crowdbotics.com>
2021-12-10 10:53:36 +0300

    3

--- blog/tests.py
+++ blog/tests.py
@@ -6,12 +6,12 @@ def test_my():
     assert 1 == 1
 
 
-class ModelTesting(TestCase):
-
-    def setUp(self):
-        self.blog = Post.objects.create(title='django', author='django', slug='django')
-
-    def test_post_model(self):
-        d = self.blog
-        self.assertTrue(isinstance(d, Post))
-        self.assertEqual(str(d), 'django')
+# class ModelTesting(TestCase):
+#
+#     def setUp(self):
+#         self.blog = Post.objects.create(title='django', author='django', slug='django')
+#
+#     def test_post_model(self):
+#         d = self.blog
+#         self.assertTrue(isinstance(d, Post))
+#         self.assertEqual(str(d), 'django')
