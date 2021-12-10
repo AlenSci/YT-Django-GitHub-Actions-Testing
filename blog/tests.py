247ee94fcf310128840ff6ffcdbb25b06355f367
AlenSci <ali.alkaraawi@crowdbotics.com>
2021-12-10 10:55:49 +0300

    0

--- blog/tests.py
+++ blog/tests.py
@@ -2,16 +2,18 @@ from django.test import TestCase
 from . models import Post
 
 
-def test_my():
-    assert 1 == 1
 
 
-# class ModelTesting(TestCase):
-#
-#     def setUp(self):
-#         self.blog = Post.objects.create(title='django', author='django', slug='django')
-#
-#     def test_post_model(self):
-#         d = self.blog
-#         self.assertTrue(isinstance(d, Post))
-#         self.assertEqual(str(d), 'django')
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
+
+    def test_my(self):
+        assert 1 == 1
